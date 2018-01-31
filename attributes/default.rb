42efbeb25170d8a1c7707cd4b6a790ec8158b09e
euonymus <euonymus0220@gmail.com>
2018-01-31 14:16:55 +0900

    try

---
+++ attributes/default.rb
@@ -0,0 +1,29 @@
+default[:cakephp][:app_name]     = 'lampapp'
+
+default[:cakephp][:www_root]     = "/var/www"
+default[:cakephp][:app_root]     = "#{node[:cakephp][:www_root]}/#{node[:cakephp][:app_name]}"
+
+default[:cakephp][:cake_source]  = "/vagrant/src/#{node[:cakephp][:app_name]}"
+
+default[:cakephp][:shell_base]  = '/usr/local/bin'
+
+# The path to the data_bag_key on the virtualbox server
+default[:cakephp][:secretpath] = "/vagrant/src/secrets/data_bag_key"
+
+# look for secret in file pointed to with cakephp attribute :secretpath
+data_bag_secret = Chef::EncryptedDataBagItem.load_secret("#{node[:cakephp][:secretpath]}")
+
+# Set MySQL info from data_bag
+mysqlinfo_creds = Chef::EncryptedDataBagItem.load("envs", "mysql", data_bag_secret)
+if data_bag_secret && mysql_envs = mysqlinfo_creds[node.chef_environment]
+  default[:cakephp][:db_name]      = mysql_envs['db_name']
+  default[:cakephp][:db_user]      = mysql_envs['db_user']
+  default[:cakephp][:testdb_name]  = mysql_envs['testdb_name']
+  default[:cakephp][:testdb_user]  = mysql_envs['testdb_user']
+end
+# Set MySQL passwords from data_bag
+mysql_creds = Chef::EncryptedDataBagItem.load("passwords", "mysql", data_bag_secret)
+if data_bag_secret && mysql_passwords = mysql_creds[node.chef_environment]
+  default[:cakephp][:db_password_root] = mysql_passwords['root']
+  default[:cakephp][:db_password] = mysql_passwords['app']
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,59 @@
 # Cookbook Name:: cakephp
 # Recipe:: default
 #
-# Copyright (C) 2018 YOUR_NAME
+# Copyright (C) 2018 euonymus
 #
 # All rights reserved - Do Not Redistribute
 #
+link "/var/opt/host.php" do
+  to node[:cakephp][:app_root] + "/config/host.php." + node.chef_environment
+end
+
+
+# delete a tmp directory
+directory node[:cakephp][:cake_source] + "/tmp" do
+  recursive true
+  action :delete
+end
+
+# create a tmp directory
+directory node[:cakephp][:cake_source] + '/tmp' do
+  owner "www-data"
+  group "www-data"
+  mode "0775"
+  action :create
+end
+
+# delete a logs directory
+directory node[:cakephp][:cake_source] + "/logs" do
+  recursive true
+  action :delete
+end
+
+# create a logs directory
+directory node[:cakephp][:cake_source] + '/logs' do
+  owner "www-data"
+  group "www-data"
+  mode "0775"
+  action :create
+end
+
+
+# Build app.php files
+template node[:cakephp][:cake_source] + '/config/app.php' do
+  source 'app.php.erb'
+  owner "www-data"
+  group "www-data"
+  mode "755"
+  variables({
+     :app_name      => node[:cakephp][:app_name],
+     :login         => node[:cakephp][:db_user],
+     :database      => node[:cakephp][:db_name],
+     :password      => node[:cakephp][:db_password],
+  })
+end
+
+# Database Table creation
+execute "create_tables" do
+  command 'cd ' + node[:cakephp][:cake_source] + " && bin/cake migrations migrate"
+end
